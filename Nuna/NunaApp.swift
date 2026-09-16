66b1fe51dfd51a2193f73995fc9c48cbc69e4c36
Alexandre Junior <its_alejunior1997@icloud.com>
2026-09-16 09:34:03 -0300

    Initial Commit

---
+++ Nuna/NunaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NunaApp.swift
+//  Nuna
+//
+//  Created by Alexandre Junior on 16/09/26.
+//
+
+import SwiftUI
+
+@main
+struct NunaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
